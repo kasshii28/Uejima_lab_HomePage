ae73448d72c60908fb5d3479c392c4b4a67d8ac1
kasshii28 <gp22a043@oecu.jp>
2025-06-21 02:19:30 +0900

    refactor:formatの修正

--- src/components/CreateGraph.tsx
+++ src/components/CreateGraph.tsx
@@ -7,27 +7,27 @@ import ForceGraph3D from "react-force-graph-3d";
 import type { ForceGraphMethods } from "react-force-graph-3d";
 
 export const CreateBloomGraph = () => {
-  const graphRef = useRef<
-    ForceGraphMethods<NodeObject, LinkObject> | undefined
-  >(undefined);
+	const graphRef = useRef<
+		ForceGraphMethods<NodeObject, LinkObject> | undefined
+	>(undefined);
 
-  useEffect(() => {
-    const bloomPass = new UnrealBloomPass();
-    bloomPass.threshold = 0;
-    bloomPass.strength = 4;
-    bloomPass.radius = 1;
-    graphRef.current?.postProcessingComposer().addPass(bloomPass);
-  }, []);
+	useEffect(() => {
+		const bloomPass = new UnrealBloomPass();
+		bloomPass.threshold = 0;
+		bloomPass.strength = 4;
+		bloomPass.radius = 1;
+		graphRef.current?.postProcessingComposer().addPass(bloomPass);
+	}, []);
 
-  return (
-    <ForceGraph3D
-      ref={graphRef}
-      backgroundColor="#000003"
-      graphData={data}
-      nodeLabel="id"
-      nodeAutoColorBy="group"
-      enableNodeDrag={false}
-      showNavInfo={false}
-    />
-  );
+	return (
+		<ForceGraph3D
+			ref={graphRef}
+			backgroundColor="#000003"
+			graphData={data}
+			nodeLabel="id"
+			nodeAutoColorBy="group"
+			enableNodeDrag={false}
+			showNavInfo={false}
+		/>
+	);
 };
